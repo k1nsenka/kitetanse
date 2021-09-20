e9ea9d4cd4d674c0e02d1e9d74f60c92c3e096ae
Tatsuaki Nemoto <tatsuaki.capricorni@gmail.com>
2021-09-20 19:49:06 +0900

    linkを修正した

--- components/Menu.js
+++ components/Menu.js
@@ -5,12 +5,12 @@ export default function Menu(){
     <div className='container'>
       <nav>
         <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/gaget">Gaget</a></li>
-          <li><a href="/life">Life</a></li>
-          <li><a href="/travel">Travel</a></li>
-          <li><a href="/develop">Develop</a></li>
-          <li><a href="/about">About us</a></li>
+          <li><Link href={`/`}><a>Home</a></Link></li>
+          <li><Link href={`/gaget`}><a>Gaget</a></Link></li>
+          <li><Link href={`/life`}><a>Life</a></Link></li>
+          <li><Link href={`/travel`}><a>Travel</a></Link></li>
+          <li><Link href={`/develop`}><a>Develop</a></Link></li>
+          <li><Link href={`/about`}><a>About us</a></Link></li>
         </ul>
       </nav>
     </div>
